49c94662b8848e071f55597d46257d35932b1130
sfwn <sfwnlin@gmail.com>
2016-09-07 10:57:35 +0800

    fix: email regexp

--- boot-common/src/main/java/com/github/sfwn/boot/common/RegexUtil.kt
+++ boot-common/src/main/java/com/github/sfwn/boot/common/RegexUtil.kt
@@ -4,7 +4,7 @@ package com.github.sfwn.boot.common
  * Created by sfwn on 9/6/16.
  */
 object RegexUtil {
-    private val EMAIL_REGEX = """..*@..*\..*""".toRegex()
+    private val EMAIL_REGEX = """.+@.+\..+""".toRegex()
     private val MOBILE_REGEX = """(^(13\d|14[57]|15[^4,\D]|17[13678]|18\d)\d{8}|170[^346,\D]\d{7})$""".toRegex()
 
     fun isEmail(email: String): Boolean {
@@ -17,5 +17,5 @@ object RegexUtil {
 }
 
 fun main(args: Array<String>) {
-    println(RegexUtil.isEmail("465755864@qq.com"))
+    println(RegexUtil.isEmail("kwm@terminus.io"))
 }
